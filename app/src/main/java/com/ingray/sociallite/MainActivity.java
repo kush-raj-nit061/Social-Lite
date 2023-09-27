c0d9278f517738e08826ded8874bfc23e8bd5634
kush-raj-nit061 <kushpvt1@gmail.com>
2023-09-28 00:23:51 +0530

    Task To Each Developer Assigned

--- app/src/main/java/com/ingray/sociallite/MainActivity.java
+++ app/src/main/java/com/ingray/sociallite/MainActivity.java
@@ -2,13 +2,92 @@ package com.ingray.sociallite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv,tv1,tv2,tv3,tv4,tv5,tv6,tv7,tv8,tv9,tv10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialisation();
+        callOnClickListeners();
+
+
+    }
+
+    private void initialisation() {
+        tv = findViewById(R.id.tv_go);
+        tv1 = findViewById(R.id.tv_go1);
+        tv2 = findViewById(R.id.tv_go2);
+        tv3 = findViewById(R.id.tv_go3);
+        tv4 = findViewById(R.id.tv_go4);
+        tv5 = findViewById(R.id.tv_go5);
+        tv6 = findViewById(R.id.tv_go6);
+    }
+
+    private void callOnClickListeners() {
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, WelcomePage.class);
+                startActivity(i);
+
+            }
+        });
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(i);
+
+            }
+        });
+        tv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(i);
+
+            }
+        });
+        tv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(i);
+
+            }
+        });
+        tv4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, FeaturedActivity.class);
+                startActivity(i);
+
+            }
+        });
+        tv5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CollaborationActivity.class);
+                startActivity(i);
+
+            }
+        });
+        tv6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NotificationsActivity.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
